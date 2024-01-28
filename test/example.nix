5baef34d138feeeb265669790eb558be2f973abd
Maciej Krüger <mkrueger@mgit.at>
2024-01-28 16:55:17 +0100

    add failing nginx

--- test/example.nix
+++ test/example.nix
@@ -1,5 +1,14 @@
 { config, pkgs, lib, ... }: {
   nix-unify = {
     modules.shareNftables.enable = true;
+    modules.shareSystemd.units = [ "nginx.service" ];
+  };
+
+  services.nginx = {
+    enable = true;
+    virtualHosts.example = {
+      default = true;
+      root = "/var/www";
+    };
   };
 }
