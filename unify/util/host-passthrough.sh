1c0c8b51b160fec2063f3aa90b96d602f605dcf1
Maciej Krüger <mkrueger@mgit.at>
2024-01-29 02:30:49 +0100

    host-passthrough: add fallback path

--- unify/util/host-passthrough.sh
+++ unify/util/host-passthrough.sh
@@ -2,10 +2,15 @@
 
 set -eu
 
+export PATH="x"
 source /etc/environment # load system path
 # banish nix
 PATH=${PATH//":/run/current-system/sw/bin"/""}
 export PATH="$PATH"
+
+if [ "$PATH" = "x" ]; then
+  export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+fi
 
 # call exec
 exec "@exec@" "$@"
