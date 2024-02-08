4fb1c5d7a69873cd4c28c51a83f21b68343759af
Maciej Krüger <mkrueger@mgit.at>
2024-02-08 10:06:53 +0100

    oil: use v0.20.0 or higher oil

--- unify/oils.nix
+++ unify/oils.nix
@@ -1,21 +1,25 @@
-{ lib
-, stdenv
-, pkgs
-}:
+{ stdenv, lib, fetchurl, symlinkJoin, withReadline ? true, readline }:
 
+let
+  readline-all = symlinkJoin {
+    name = "readline-all"; paths = [ readline readline.dev ];
+  };
+in
 stdenv.mkDerivation rec {
-  pname = "oils-ci";
-  version = "6015"; # Version of the github job...
+  pname = "oil";
+  version = "0.20.0";
 
-  src = pkgs.fetchurl {
-    # url = "http://travis-ci.oilshell.org/github-jobs/6015/cpp-tarball.wwz/_release/oils-for-unix.tar";
-    url = "https://mkg20001.io/tmp/f7qmpbj3dmajcxki05b6gc0hxl0967p8-oils-for-unix.tar";
-    hash = "sha256-TFbzcbdpqKw7J12iNtKPgJQ2g/8Ed84edcKeDfGdDuI=";
+  src = fetchurl {
+    url = "https://www.oilshell.org/download/oils-for-unix-${version}.tar.gz";
+    hash = "sha256-d4BIRj8bPyd7awZyJPlZYBwr+o82IKGh4y4/urOYOxc=";
   };
 
-  patchPhase = ''
-    patchShebangs build
+  postPatch = ''
     patchShebangs _build
+  '';
+
+  preInstall = ''
+    mkdir -p $out/bin
   '';
 
   buildPhase = ''
@@ -26,14 +30,37 @@ stdenv.mkDerivation rec {
     ./install
   '';
 
+  strictDeps = true;
+  buildInputs = lib.optional withReadline readline;
+  # As of 0.19.0 the build generates an error on MacOS (using clang version 16.0.6 in the builder),
+  # whereas running it outside of Nix with clang version 15.0.0 generates just a warning. The shell seems to
+  # work just fine though, so we disable the error here.
+  env.NIX_CFLAGS_COMPILE = lib.optionalString stdenv.cc.isClang "-Wno-error=incompatible-function-pointer-types";
+  configureFlags = [
+    "--datarootdir=${placeholder "out"}"
+  ] ++ lib.optionals withReadline [
+    "--with-readline"
+    "--readline=${readline-all}"
+  ];
+
+  # Stripping breaks the bundles by removing the zip file from the end.
   dontStrip = true;
 
-  meta = with lib; {
-    description = "Oils for Unix. The C++ build directly from the CI!";
-    homepage = "https://oilshell.org";
-    #changelog = "https://github.com/mtkennerly/ludusavi/blob/v${version}/CHANGELOG.md";
-    license = licenses.asl20;
-    #maintainers = with maintainers; [ melkor333 ];
-    #mainProgram = "ysh";
+  meta = {
+    description = "A new unix shell";
+    homepage = "https://www.oilshell.org/";
+
+    license = with lib.licenses; [
+      psfl # Includes a portion of the python interpreter and standard library
+      asl20 # Licence for Oil itself
+    ];
+
+    platforms = lib.platforms.all;
+    maintainers = with lib.maintainers; [ lheckemann alva mkg20001 ];
+    changelog = "https://www.oilshell.org/release/${version}/changelog.html";
+  };
+
+  passthru = {
+    shellPath = "/bin/osh";
   };
 }
