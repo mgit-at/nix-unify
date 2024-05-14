9d69e87c0a61e2573effe09ca55aced642b4f35f
Maciej Krüger <mkrueger@mgit.at>
2024-05-14 23:18:06 +0200

    test/test-deployment: fix key permissions

--- test/test-deployment.sh
+++ test/test-deployment.sh
@@ -11,6 +11,8 @@ else
   OS="$OSLIST"
 fi
 
+chmod 600 "$SELF/id_ed25519_dev"
+
 for os in $OS; do
   IP=$(get_ip "unify-$os")
   NIX_SSHOPTS="-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o IdentitiesOnly=yes -i $SELF/id_ed25519_dev" nixos-rebuild --flake ".#test" --target-host "root@$IP" switch --show-trace
